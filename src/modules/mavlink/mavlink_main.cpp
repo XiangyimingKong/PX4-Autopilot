caf33fd87f176b350213b2d2989a670785a15855
yiming <yiming@autonosky.com>
2026-03-30 14:32:52 +0200

    init cmd and stream rate adjusted

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1653,7 +1653,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 
 	/* fallthrough */
 	case MAVLINK_MODE_CUSTOM:
-		configure_stream_local("TEST_COUNTER", 40.0f);
+		configure_stream_local("TEST_COUNTER", 20.0f);
 		configure_stream_local("DROPBOX_STATUS", 10.0f);
 		//stream nothing
 		break;
